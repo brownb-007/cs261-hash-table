0388644c43cd319d0e8c0ee425c5da1197bf3eb4
Brayden Brown <braydenbrown@gmail.com>
2020-10-11 14:10:49 -0700

    Instantiation and size passed

--- hash_table.py
+++ hash_table.py
@@ -1,9 +1,11 @@
 # HashTable: An unordered key-value data structure providing O(1) store, retrieve
 # search and delete operations.
 # Your implementation should pass the tests in test_hash_table.py.
-# YOUR NAME
+# Brayden Brown
 
 
 class HashTable:
 
+    def __init__(self, size=10):
+        self.size = size
     pass
