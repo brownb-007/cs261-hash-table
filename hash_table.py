1067137d0550b76525f2879f7e9e8d4609df5697
Brayden Brown <braydenbrown@gmail.com>
2020-10-14 16:33:23 -0700

    __setitem__ test passed

--- hash_table.py
+++ hash_table.py
@@ -8,8 +8,16 @@ class HashTable:
 
     def __init__(self, size=10):
         self.size = size
-        self.data = [[] for i in range(self.size)]
+        self.data = [[] for i in range(size)]
 
     def __setitem__(self, key, value):
-        
+        index = self._hashed(key)
+        for key_and_value in self.data[index]:
+            if key_and_value[0] == key:
+                key_and_value[1] = value
+        self.data[index].append([key, value])
+
+    def _hashed(self, key):
+        return hash(key) % self.size
+
     pass
